ff2cf1441e012de7f6be497b8a95e74a3b3627ef
Rob <Talon@Emmas-MacBook-Pro.local>
2022-06-16 10:38:45 +0100

    Altered the endorsement buttons to one button that alternates between the 2 states

--- src/Components/Endorsements.js
+++ src/Components/Endorsements.js
@@ -5,7 +5,6 @@ import { patchEndorsements, getIndividualReview } from "../Utils/Api-Calls";
 const Endorsements = ({ endorsements, review_id }) => {
   const [currentEndorsements, setCurrentEndorsements] = useState(endorsements);
   const [error, setError] = useState(null);
-  const [clicked, setClicked] = useState(false);
 
   useEffect(() => {
     getIndividualReview(review_id).then(({ votes }) => {
@@ -14,19 +13,33 @@ const Endorsements = ({ endorsements, review_id }) => {
   }, [review_id]);
 
   const handleClick = ({ target }) => {
+    target.value === "1"
+      ? (target.innerText = "Unendorse this Review")
+      : (target.innerText = "Endorse this Review");
+
     target.disabled = true;
-    setClicked(true);
     setCurrentEndorsements(
       (currentEndorsements) => currentEndorsements + +target.value
     );
-    patchEndorsements(review_id, +target.value).catch((error) => {
-      setCurrentEndorsements(
-        (currentEndorsements) => currentEndorsements - +target.value
-      );
-      setError(
-        "Your endorsement of this review has not acsecnded to the ether, please try again."
-      );
-    });
+    patchEndorsements(review_id, +target.value)
+      .then(() => {
+        target.value === "1" ? (target.value = -1) : (target.value = 1);
+
+        target.disabled = false;
+      })
+      .catch((error) => {
+        setCurrentEndorsements(
+          (currentEndorsements) => currentEndorsements - +target.value
+        );
+        target.value === "1"
+          ? (target.innerText = "Unendorse this Review")
+          : (target.innerText = "Endorse this Review");
+        target.disabled = false;
+
+        setError(
+          "Your endorsement of this review has not acsecnded to the ether, please try again."
+        );
+      });
   };
 
   if (error) return <p>{error}</p>;
@@ -37,9 +50,6 @@ const Endorsements = ({ endorsements, review_id }) => {
       </p>
       <button onClick={handleClick} value={1}>
         Endorse this Review
-      </button>
-      <button onClick={handleClick} value={-1}>
-        Unendorse this Review
       </button>
     </>
   );
